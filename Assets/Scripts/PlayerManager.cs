f8f5215e773b0614b12171abffd7aca24534449c
leonmatte <leonel.matte.7e4@itb.cat>
2021-05-26 16:44:15 +0200

    Cambios3 PlayerManager

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,4 +1,7 @@
+using System.Collections;
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -7,17 +10,26 @@ public class PlayerManager : MonoBehaviour
     public float CurrentLapTime { get; private set; }
     public int CurrentLap { get; private set; }
 
-    private float _lapTimer;
     private int _lastCheckPointPassed;
-
     private float _lapTimerTimestamp;
-
     private Transform _checkpointsParent;
     private int _checkpointCount;
-    private int _checkpointLayer;
+    [UsedImplicitly] private int _checkpointLayer;
+
+    public GameObject countDown;
+    public AudioSource getReady;
+    public AudioSource goAudio;
+    public GameObject[] cars = new GameObject[5];
+
+    private Text _text;
+    private Text _text1;
+    private Text _text2;
 
     private void Awake()
     {
+        _text2 = countDown.GetComponent<Text>();
+        _text1 = countDown.GetComponent<Text>();
+        _text = countDown.GetComponent<Text>();
         LastLapTime = 0;
         CurrentLapTime = 0;
         CurrentLap = 0;
@@ -25,6 +37,53 @@ public class PlayerManager : MonoBehaviour
         _checkpointsParent = GameObject.Find("Checkpoints").transform;
         _checkpointCount = _checkpointsParent.childCount;
         _checkpointLayer = LayerMask.NameToLayer("Checkpoint");
+    }
+
+    private void Start()
+    {
+        StartCoroutine(CountStart());
+        foreach (GameObject car in cars)
+        {
+            if (car.GetComponent<carControllerVer4>().isPlayer == false)
+            {
+                car.GetComponent<AltCarAIController>().enabled = false;
+                car.GetComponent<UIController>().enabled = false;
+            }
+
+            car.GetComponent<carControllerVer4>().enabled = false;
+            car.GetComponent<UIController>().enabled = true;
+        }
+    }
+
+    private IEnumerator CountStart()
+    {
+        yield return new WaitForSeconds(0.3f);
+        _text.text = "3";
+        getReady.Play();
+        countDown.SetActive(true);
+        yield return new WaitForSeconds(1);
+        countDown.SetActive(false);
+        _text1.text = "2";
+        getReady.Play();
+        countDown.SetActive(true);
+        yield return new WaitForSeconds(1);
+        countDown.SetActive(false);
+        _text2.text = "1";
+        getReady.Play();
+        countDown.SetActive(true);
+        yield return new WaitForSeconds(1);
+        countDown.SetActive(false);
+
+        foreach (GameObject car in cars)
+        {
+            if (car.GetComponent<carControllerVer4>().isPlayer != true)
+            {
+                car.GetComponent<AltCarAIController>().enabled = true;
+            }
+
+            car.GetComponent<carControllerVer4>().enabled = true;
+        }
+        goAudio.Play();
     }
 
     private void StartLap()
@@ -63,7 +122,7 @@ public class PlayerManager : MonoBehaviour
         }
     }
 
-    void Update()
+    private void Update()
     {
         CurrentLapTime = _lapTimerTimestamp > 0 ? Time.time - _lapTimerTimestamp : 0;
     }
