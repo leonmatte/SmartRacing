67ae6d9b79bdbabafa14f7a5ba7f890e2a2dbd82
leonmatte <leonel.matte.7e4@itb.cat>
2021-05-27 18:24:13 +0200

    Pequeño avance en lo de los tiempos

--- Assets/Scripts/Stopwatch.cs
+++ Assets/Scripts/Stopwatch.cs
@@ -7,27 +7,34 @@ public class Stopwatch : MonoBehaviour
     bool _stopwatchActive;
     float _currentTime;
     public TMP_Text currentTimeText;
-    
-    public float BestLapTime { get; private set; } = Mathf.Infinity;
-    public float LastLapTime { get; private set; }
-    public float CurrentLapTime { get; private set; }
-    public int CurrentLap { get; private set; }
+    public TMP_Text currentLap;
+    public TMP_Text lastLapTimeText;
+
+    public int CurrentLap;
 
     private int _lastCheckPointPassed;
-    private float _lapTimerTimestamp;
     private Transform _checkpointsParent;
     private int _checkpointCount;
     private int _checkpointLayer;
 
+    private readonly carControllerVer4[] _carControllers = new carControllerVer4[5];
+    private readonly AltCarAIController[] _carAIControllers = new AltCarAIController[5];
+
+    public GameObject[] cars = new GameObject[5];
+
     private void Awake()
     {
-        LastLapTime = 0;
-        CurrentLapTime = 0;
-        CurrentLap = 0;
+        CurrentLap = 1;
         _lastCheckPointPassed = 0;
         _checkpointsParent = GameObject.Find("Checkpoints").transform;
         _checkpointCount = _checkpointsParent.childCount;
         _checkpointLayer = LayerMask.NameToLayer("Checkpoint");
+
+        for (int i = 0; i < 4; i++)
+        {
+            _carControllers[i] = cars[i].GetComponent<carControllerVer4>();
+            _carAIControllers[i] = cars[i].GetComponent<AltCarAIController>();
+        }
     }
 
     void Start()
@@ -48,16 +55,17 @@ public class Stopwatch : MonoBehaviour
         currentTimeText.text = time.ToString(@"mm\:ss\:fff");
     }
 
-    public void StartStopwatch()
+    public void startTimer()
     {
         _stopwatchActive = true;
     }
 
-    public void StopStopwatch()
+    public void resetTimer()
     {
         _stopwatchActive = false;
+        _currentTime = 0;
     }
-    
+
     void OnTriggerEnter(Collider colission)
     {
         if (colission.gameObject.layer != _checkpointLayer)
@@ -69,18 +77,56 @@ public class Stopwatch : MonoBehaviour
         {
             if (_lastCheckPointPassed == _checkpointCount)
             {
-                StopStopwatch();
+                lastLapTimeText.text = _currentTime.ToString(@"mm\:ss\:fff");
+                resetTimer();
             }
 
-            if (CurrentLap == 0|| _lastCheckPointPassed == _checkpointCount)
+            if (CurrentLap == 0 || _lastCheckPointPassed == _checkpointCount)
             {
-                StartStopwatch();
+                startTimer();
+                CurrentLap++;
+                currentLap.text = currentLap.ToString();
             }
         }
 
         if (colission.gameObject.name == (_lastCheckPointPassed + 1).ToString())
         {
             _lastCheckPointPassed++;
+        }
+    }
+
+    void CarControllerSwitch(bool active)
+    {
+        switch (active)
+        {
+            case true:
+            {
+                foreach (carControllerVer4 controller in _carControllers)
+                {
+                    controller.enabled = true;
+                }
+
+                foreach (AltCarAIController aiController in _carAIControllers)
+                {
+                    aiController.enabled = true;
+                }
+
+                break;
+            }
+            case false:
+            {
+                foreach (carControllerVer4 controller in _carControllers)
+                {
+                    controller.enabled = false;
+                }
+
+                foreach (AltCarAIController aiController in _carAIControllers)
+                {
+                    aiController.enabled = false;
+                }
+
+                break;
+            }
         }
     }
 }
